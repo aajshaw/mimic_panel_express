83158bac75e5bdc5c84cd9724b8180111e0498f0
aajshaw <tony@adsaw.uk>
2022-11-23 14:31:58 +0000

    Added Route to imported items

--- mimic_panel_express/__init__.py
+++ mimic_panel_express/__init__.py
@@ -8,5 +8,5 @@ from .mimic_panel_express import Track
 from .mimic_panel_express import Stub
 from .mimic_panel_express import Turnout
 from .mimic_panel_express import Block
+from .mimic_panel_express import Route
 from .mimic_panel_express import Layout
-
