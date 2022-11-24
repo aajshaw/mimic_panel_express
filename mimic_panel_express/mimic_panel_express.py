68afa93e4caeec483149516053ebc748ed1e50b8
aajshaw <tony@adsaw.uk>
2022-11-24 18:30:44 +0000

    Added setting of defaults

--- mimic_panel_express/mimic_panel_express.py
+++ mimic_panel_express/mimic_panel_express.py
@@ -6,7 +6,73 @@ TRACK_NORMAL = 2
 SIGNAL_DANGER = 0
 SIGNAL_CLEAR = 1
 
-def get_screen_size():
+TRACK_NORMAL_COLOR = 'yellow'
+TRACK_SAFE_COLOR = 'yellow'
+TRACK_DANGER_COLOR = 'red'
+STUB_NORMAL_COLOR = 'yellow'
+STUB_SAFE_COLOR = 'yellow'
+STUB_DANGER_COLOR = 'red'
+SIGNAL_CLEAR_COLOR = 'green'
+SIGNAL_DANGER_COLOR = 'red'
+TURNOUT_NORMAL_COLOR = 'yellow'
+TURNOUT_SAFE_COLOR = 'green'
+TURNOUT_DANGER_COLOR = 'red'
+TURNOUT_POINT_COLOR = 'blue'
+BLOCK_LABEL_COLOR = 'blue'
+LAYOUT_LABEL_COLOR = 'blue'
+LAYOUT_BACKGROUND_COLOR = 'black'
+
+def set_default(item, value):
+    mpe_item = item.upper()
+    if mpe_item == 'TRACK_NORMAL_COLOR':
+        global TRACK_NORMAL_COLOR
+        TRACK_NORMAL_COLOR = value
+    elif mpe_item == 'TRACK_SAFE_COLOR':
+        global TRACK_SAFE_COLOR
+        TRACK_SAFE_COLOR = value
+    elif mpe_item == 'TRACK_DANGER_COLOR':
+        global TRACK_DANGER_COLOR
+        TRACK_DANGER_COLOR = value
+    elif mpe_item == 'STUB_NORMAL_COLOR':
+        global STUB_NORMAL_COLOR
+        STUB_NORMAL_COLOR = value
+    elif mpe_item == 'STUB_SAFE_COLOR':
+        global STUB_SAFE_COLOR
+        STUB_SAFE_COLOR = value
+    elif mpe_item == 'STUB_DANGER_COLOR':
+        global STUB_DANGER_COLOR
+        STUB_DANGER_COLOR = value
+    elif mpe_item == 'SIGNAL_CLEAR_COLOR':
+        global SIGNAL_CLEAR_COLOR
+        SIGNAL_CLEAR_COLOR = value
+    elif mpe_item == 'SIGNAL_DANGER_COLOR':
+        global SIGNAL_DANGER_COLOR
+        SIGNAL_DANGER_COLOR = value
+    elif mpe_item == 'TURNOUT_NORMAL_COLOR':
+        global TURNOUT_NORMAL_COLOR
+        TURNOUT_NORMAL_COLOR = value
+    elif mpe_item == 'TURNOUT_SAFE_COLOR':
+        global TURNOUT_SAFE_COLOR
+        TURNOUT_SAFE_COLOR = value
+    elif mpe_item == 'TURNOUT_DANGER_COLOR':
+        global TURNOUT_DANGER_COLOR
+        TURNOUT_DANGER_COLOR = value
+    elif mpe_item == 'TURNOUT_POINT_COLOR':
+        global TURNOUT_POINT_COLOR
+        TURNOUT_POINT_COLOR = value
+    elif mpe_item == 'BLOCK_LABEL_COLOR':
+        global BLOCK_LABEL_COLOR
+        BLOCK_LABEL_COLOR = value
+    elif mpe_item == 'LAYOUT_LABEL_COLOR':
+        global LAYOUT_LABEL_COLOR
+        LAYOUT_LABEL_COLOR = value
+    elif mpe_item == 'LAYOUT_BACKGROUND_COLOR':
+        global LAYOUT_BACKGROUND_COLOR
+        LAYOUT_BACKGROUND_COLOR = value
+    else:
+        raise ValueError(f'Invalid item for setting of default value: item = {item}, value = {value}')
+
+def _get_screen_size():
     layout = [[]]
     window = sg.Window('', layout, finalize = True)
     size = window.get_screen_size()
@@ -17,10 +83,10 @@ class Route:
     def __init__(self, id):
         self.id = id
         self.legs = []
-    
+
     def add(self, leg, does):
         self.legs.append(getattr(leg, does))
-        
+
     def run(self):
         for leg in self.legs:
             leg()
@@ -30,15 +96,15 @@ class Track:
         def __init__(self, item):
             self._item = item
             self._index = 0
-            
+
         def __next__(self):
             if self._index == 0:
                 self._index += 1
                 return self._item
             else:
                 raise StopIteration
-        
-    def __init__(self, id, in_point = None, out_point = None, state = TRACK_NORMAL, normal_color = 'yellow', safe_color = 'yellow', danger_color = 'red'):
+
+    def __init__(self, id, in_point = None, out_point = None, state = TRACK_NORMAL, normal_color = None, safe_color = None, danger_color = None):
         self.id = id
         if in_point and out_point:
             if in_point <= out_point:
@@ -52,27 +118,36 @@ class Track:
             self.in_point = in_point
             self.out_point = out_point
         self.state = state
-        self.normal_color = normal_color
-        self.safe_color = safe_color
-        self.danger_color = danger_color
+        if normal_color is None:
+            self.normal_color = TRACK_NORMAL_COLOR
+        else:
+            self.normal_color = normal_color
+        if safe_color is None:
+            self.safe_color = TRACK_SAFE_COLOR
+        else:
+            self.safe_color = safe_color
+        if danger_color is None:
+            self.danger_color = TRACK_DANGER_COLOR
+        else:
+            self.danger_color = danger_color
         self.graph_id = None
         self.panel = None
-    
+
     def __iter__(self):
         return self._Iterator(self)
-    
+
     def get_in_point(self):
         return self.in_point
-    
+
     def get_out_point(self):
         return self.out_point
-    
+
     def get_graph_id(self):
         return self.graph_id
-    
+
     def set_panel(self, panel):
         self.panel = panel
-    
+
     def draw(self):
         if self.in_point and self.out_point:
             if self.state == TRACK_NORMAL:
@@ -84,17 +159,17 @@ class Track:
             else:
                 color = self.danger_color
             self.graph_id = self.panel.draw_line(self.in_point, self.out_point, color = color, width = 3)
-    
+
     def clicked(self, figures):
         pass
-    
+
     def erase(self):
         self.panel.delete_figure(self.graph_id)
-    
+
     def safe(self):
         self.state = TRACK_SAFE
         self.panel.tk_canvas.itemconfigure(self.graph_id, fill = self.safe_color)
-            
+
     def danger(self):
         self.state = TRACK_DANGER
         self.panel.tk_canvas.itemconfigure(self.graph_id, fill = self.danger_color)
@@ -104,33 +179,39 @@ class Stub:
         def __init__(self, item):
             self._item = item
             self._index = 0
-            
+
         def __next__(self):
             if self._index == 0:
                 self._index += 1
                 return self._item
             else:
                 raise StopIteration
-        
-    def __init__(self, id, in_point = None, out_point = None, state = TRACK_NORMAL, normal_color = 'yellow', safe_color = 'green', danger_color = 'red'):
+
+    def __init__(self, id, in_point = None, out_point = None, state = TRACK_NORMAL, normal_color = None, safe_color = None, danger_color = None):
         self.id = id
+        if normal_color is None:
+            normal_color = STUB_NORMAL_COLOR
+        if safe_color is None:
+            safe_color = STUB_SAFE_COLOR
+        if danger_color is None:
+            danger_color = STUB_DANGER_COLOR
         self.track = Track('Track-' + id, in_point, out_point, state, normal_color, safe_color, danger_color)
-    
+
     def __iter__(self):
         return self._Iterator(self)
-    
+
     def get_in_point(self):
         return self.track.get_in_point()
-    
+
     def set_panel(self, panel):
         self.track.set_panel(panel)
-    
+
     def draw(self):
         self.track.draw()
-        
+
     def clicked(self, figures):
         pass
-    
+
     def erase(self):
         self.track.erase()
 
@@ -139,43 +220,49 @@ class Signal:
         def __init__(self, item):
             self._item = item
             self._index = 0
-            
+
         def __next__(self):
             if self._index == 0:
                 self._index += 1
                 return self._item
             else:
                 raise StopIteration
-        
-    def __init__(self, id, position, state = SIGNAL_DANGER, clear_color = 'green', danger_color = 'red'):
+
+    def __init__(self, id, position, state = SIGNAL_DANGER, clear_color = None, danger_color = None):
         self.id = id
         self.position = position
         self.state = state
-        self.clear_color = clear_color
-        self.danger_color = danger_color
+        if clear_color is None:
+            self.clear_color = SIGNAL_CLEAR_COLOR
+        else:
+            self.clear_color = clear_color
+        if danger_color is None:
+            self.danger_color = SIGNAL_DANGER_COLOR
+        else:
+            self.danger_color = danger_color
         self.graph_id = None
         self.panel = None
-    
+
     def __iter__(self):
         return self._Iterator()
-    
+
     def clear(self):
         self.state = SIGNAL_CLEAR
         self.panel.tk_canvas.itemconfigure(self.graph_id, fill = self.clear_color, outline = self.clear_color)
-    
+
     def danger(self):
         self.state = SIGNAL_DANGER
         self.panel.tk_canvas.itemconfigure(self.graph_id, fill = self.danger_color, outline = self.danger_color)
-        
+
     def toggle(self):
         if self.state == SIGNAL_CLEAR:
             self.danger()
         elif self.state == SIGNAL_DANGER:
             self.clear()
-    
+
     def set_panel(self, panel):
         self.panel = panel
-    
+
     def draw(self):
         if self.position:
             if self.state == SIGNAL_DANGER:
@@ -185,11 +272,11 @@ class Signal:
             else:
                 color = self.danger_color
             self.graph_id = self.panel.draw_circle(self.position, 10, fill_color = color, line_color = color)
-    
+
     def clicked(self, figures):
         if self.graph_id in figures:
             self.toggle()
-            
+
     def erase(self):
         self.panel.delete_figure(self.graph_id)
 
@@ -198,17 +285,23 @@ class Turnout:
         def __init__(self, item):
             seld._item = item
             self._index = 0
-            
+
         def __next__(self):
             if self._index == 0:
                 self._index += 1
                 return self._item
             else:
                 raise StopIteration
-        
-    def __init__(self, id, position, entry = None, normal = None, reverse = None, normal_color = 'yellow', safe_color = 'yellow', danger_color = 'red', point_color = 'blue'):
+
+    def __init__(self, id, position, entry = None, normal = None, reverse = None, normal_color = None, safe_color = None, danger_color = None, point_color = None):
         self.id = id
         self.position = position
+        if normal_color is None:
+            normal_color = TURNOUT_NORMAL_COLOR
+        if safe_color is None:
+            safe_color = TURNOUT_SAFE_COLOR
+        if danger_color is None:
+            danger_color = TURNOUT_DANGER_COLOR
         if entry:
             self.entry_point = entry
         else:
@@ -224,55 +317,58 @@ class Turnout:
         else:
             self.reverse_point = (self.position[0] + 100, self.position[1] - 50)
         self.reverse_track = Track('Track-Reverse-' + id, position, self.reverse_point, TRACK_DANGER, normal_color, safe_color, danger_color)
-        self.point_color = point_color
+        if point_color is None:
+            self.point_color = TURNOUT_POINT_COLOR
+        else:
+            self.point_color = point_color
         self.point_circle_graph_id = None
         self.state = 'N'
         self.panel = None
 
     def __iter__(self):
         return self._Iterator()
-    
+
     def get_entry_point(self):
         return self.entry_point
-    
+
     def get_normal_point(self):
         return self.normal_point
-    
+
     def get_reverse_point(self):
         return self.reverse_point
-    
+
     def normal(self):
         self.normal_track.safe()
         self.reverse_track.danger()
         self.state = 'N'
-    
+
     def reverse(self):
         self.normal_track.danger()
         self.reverse_track.safe()
         self.state = 'R'
-    
+
     def toggle(self):
         if self.state == 'R':
             self.normal()
         elif self.state == 'N':
             self.reverse()
-    
+
     def set_panel(self, panel):
         self.panel = panel
         self.entry_track.set_panel(panel)
         self.normal_track.set_panel(panel)
         self.reverse_track.set_panel(panel)
-    
+
     def draw(self):
         self.entry_track.draw()
         self.normal_track.draw()
         self.reverse_track.draw()
         self.point_circle_graph_id = self.panel.draw_circle(self.position, 10, fill_color = self.point_color, line_color = self.point_color)
-    
+
     def clicked(self, figures):
         if self.point_circle_graph_id in figures or self.entry_track.get_graph_id() in figures or self.normal_track.get_graph_id() in figures or self.reverse_track.get_graph_id() in figures:
             self.toggle()
-        
+
     def erase(self):
         self.entry_track.erase()
         self.normal_track.erase()
@@ -284,7 +380,7 @@ class Block:
         def __init__(self, items):
             self._items = items
             self._index = 0
-            
+
         def __next__(self):
             if self._index < len(self._items):
                 result = self._items[self._index]
@@ -292,60 +388,66 @@ class Block:
                 return result
             else:
                 raise StopIteration
-        
-    def __init__(self, id, label = None, label_position = None, label_font_size = 20, label_color = 'blue'):
+
+    def __init__(self, id, label = None, label_position = None, label_font_size = 20, label_color = None):
         self.id = id
         self.label = label
         self.label_position = label_position
         self.label_font_size = label_font_size
-        self.label_color = label_color
+        if label_color is None:
+            self.label_color = BLOCK_LABEL_COLOR
+        else:
+            self.label_color = label_color
         self.items = []
         self.panel = None
-        
+
     def __iter__(self):
         return self._Iterator(self.items)
-    
-#    def __length__(self):
-#        return len(self.items)
-    
+
     def __getitem__(self, key):
         print('__getitem__() key:', key)
         return self.find_element(key)
-    
+
     def find_element(self, key):
         for item in self.items:
             if item.id == key:
                 return item
         raise KeyError(key)
-    
+
     def add(self, item):
         self.items.append(item)
-        
+
     def set_panel(self, panel):
         self.panel = panel
         for item in self.items:
             item.set_panel(panel)
-    
+
     def draw(self):
         if self.label:
             self.panel.draw_text(self.label, self.label_position, color = self.label_color, font = ('', self.label_font_size))
         for item in self.items:
             item.draw()
-    
+
     def clicked(self, figures):
         for item in self.items:
             item.clicked(figures)
-    
+
     def erase():
         for item in self.items:
             item.erase()
 
 class Layout:
-    def __init__(self, label, label_font_size = 30, label_color = 'blue', background_color = 'black', height = 300, width = 1200, clickable_panel = True, item_buttons = True, route_buttons = True, exit_button = True):
+    def __init__(self, label, label_font_size = 30, label_color = None, background_color = None, height = 300, width = 1200, clickable_panel = True, item_buttons = True, route_buttons = True, exit_button = True):
         self.label = label
         self.label_font_size = label_font_size
-        self.label_color = label_color
-        self.background_color = background_color
+        if label_color is None:
+            self.label_color = LAYOUT_LABEL_COLOR
+        else:
+            self.label_color = label_color
+        if background_color is None:
+            self.background_color = LAYOUT_BACKGROUND_COLOR
+        else:
+            self.background_color = background_color
         self.width = width
         self.height = height
         self.item_buttons = item_buttons
@@ -355,23 +457,23 @@ class Layout:
         self.blocks = []
         self.routes = []
         self.panel = None
-    
+
     def add_block(self, block):
         self.blocks.append(block)
-        
+
     def add_route(self, route):
         self.routes.append(route)
-        
+
     def add(self, item):
         if isinstance(item, Route):
             self.add_route(item)
         else:
             self.add_block(item)
-    
+
     def run(self, initial_route = None):
-        screen_size = get_screen_size()
+        screen_size = _get_screen_size()
         canvas_size = (screen_size[0] - 100, screen_size[1] - 250)
-        
+
         buttons = []
         if self.item_buttons:
             item_buttons = []
@@ -384,7 +486,7 @@ class Layout:
                             item_buttons.append(sg.Button(item.id, font = ('', 20), key = '+item+' + item.id))
             if len(item_buttons):
                 buttons.append(item_buttons)
-                
+
         if self.route_buttons:
             route_buttons = []
             for route in self.routes:
@@ -393,19 +495,19 @@ class Layout:
                 buttons.append(route_buttons)
         if self.exit_button:
             buttons.append([sg.Button('Exit', font = ('', 20))])
-        
+
         layout = [ [sg.Text(self.label, font = ('', self.label_font_size), justification = 'center', expand_x = True)],
                    [sg.Graph(canvas_size = canvas_size, graph_bottom_left = (0, 0), graph_top_right = (self.width, self.height), background_color = self.background_color, enable_events = True, key = 'panel', expand_x = True)] ]
         if self.item_buttons or self.route_buttons or self.exit_button:
             layout.append([sg.Column(buttons, expand_x = True, element_justification = 'center')])
-        
+
         window = sg.Window('Mimic Panel', layout, size = screen_size, finalize = True, no_titlebar = True)
 
         panel = window['panel']
-        
+
         for block in self.blocks:
             block.set_panel(panel)
-            
+
         for block in self.blocks:
             block.draw()
 
@@ -418,7 +520,7 @@ class Layout:
                     if route.id == initial_route:
                         route.run()
                         break
-        
+
         while True:
             event, values = window.read()
             if event == sg.WIN_CLOSED:
@@ -453,23 +555,25 @@ class Layout:
                         if done:
                             break
         window.close()
-        
+
     def __getitem__(self, key):
         return self.find_element(key)
-    
+
     def find_element(self, key):
         for block in blocks:
             if block.id == key:
                 return block
         raise KeyError(key)
-    
+
     def draw(self):
         for block in self.blocks:
             block.draw()
 
 if __name__ == '__main__':
     # from mimic_panel_express import Track, Stub, Turnout, Signal, Block, Route, Layout
-    
+
+#    set_default('LAYOUT_BACKGROUND_COLOR', 'white')
+
     west_entry_track = Track('West Entry', (25, 200), (75, 200))
     starter_signal = Signal('Starter', (75, 225))
     west_turnout = Turnout('West Turnout', position = (150, 200))
@@ -512,13 +616,13 @@ if __name__ == '__main__':
     main_line_from_platform.add(west_turnout, 'normal')
     main_line_from_platform.add(east_turnout, 'normal')
     main_line_from_platform.add(starter_signal, 'clear')
-    
+
     loop_line = Route('Loop Line')
     loop_line.add(starter_signal, 'danger')
     loop_line.add(west_turnout, 'reverse')
     loop_line.add(east_turnout, 'reverse')
     loop_line.add(south_turnout, 'normal')
-    
+
     goods_line = Route('Goods Line')
     goods_line.add(starter_signal, 'danger')
     goods_line.add(south_turnout, 'reverse')
