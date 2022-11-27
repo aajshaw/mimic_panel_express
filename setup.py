59cbbc0503a8fa1f404a47ab510ab881f7e3c17e
aajshaw <tony@adsaw.uk>
2022-11-27 17:14:10 +0000

    Adding a test setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setuptools.setup(
+    name="mimic_panel_express",
+    version="0.1.0",
+    author="Anthony Shaw",
+    author_email="tony@adshaw.uk",
+    description="Simple mimic panel development",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="model railway mimic panel",
+    url="https://github.com/aajshaw/mimic_panel_express",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Topic :: SOFTWARE DEVELOPMENT :: LIBRARIES :: PYTHON_MODULES",
+        "Operating System :: OS Independent"
+    ),
+)
